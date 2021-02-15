f23a8b7325b45cf057161004d87a4dd86748b563
Pau Salto <pausaltom@gmail.com>
2021-02-15 21:16:53 +0100

    Acabando el ejercicio, falta comprobar id válida, probar update y save

--- src/actividadxmldom/ActividadXMLDOM.java
+++ src/actividadxmldom/ActividadXMLDOM.java
@@ -6,6 +6,7 @@
 package actividadxmldom;
 
 import java.io.File;
+import java.util.Scanner;
 import org.w3c.dom.Document;
 
 /**
@@ -18,13 +19,31 @@ public class ActividadXMLDOM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner leerTeclado = new Scanner(System.in);
         // TODO code application logic here
         File fichero = new File("./src/actividadxmldom/Comida.xml");
         
         XMLconDOM xmldom = new XMLconDOM(fichero);
         
        Document doc =xmldom.abrirXML(fichero);
-       
+       boolean repetir = false;
+        do {            
+            System.out.println("Desea modificar el archivo XML?(s/n)");
+            String respuesta = leerTeclado.nextLine().toLowerCase();
+            if (respuesta == "s" | respuesta == "n") {
+                repetir = false;
+                if (respuesta == "s") {
+                    System.out.println("Porfavor introduzca el id del registro que quiere cambiar:");
+                    int id = leerTeclado.nextInt();
+                }else{
+                    return ;
+                }
+                
+            } else {
+                System.out.println("Respuesta no válida");
+                repetir = true;
+            }
+        } while (repetir);
        String salida =xmldom.recorrerDOMyMostrar(doc);
        
         System.out.println(salida);
--- src/actividadxmldom/XMLconDOM.java
+++ src/actividadxmldom/XMLconDOM.java
@@ -8,7 +8,13 @@ package actividadxmldom;
 import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -23,8 +29,6 @@ public class XMLconDOM {
     public XMLconDOM(File fichero) {
         this.fichero = fichero;
     }
-
-    
 
     public void setFichero(File fichero) {
         this.fichero = fichero;
@@ -57,34 +61,33 @@ public class XMLconDOM {
         String datos_nodo[] = null;
         String salida = "";
         Node node;
-        int contador=1;
         //Obtiene el primero nodo del DOM (primer hijo)
         Node raiz = doc.getFirstChild();
-        
+
         //Obtiene una lista de nodos con todos los nodos hijo del raíz.
         NodeList nodelist = raiz.getChildNodes();
-        
+
         //Procesa los nodos hijo
         for (int i = 0; i < nodelist.getLength(); i++) {
             node = nodelist.item(i);
-            
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 //Es un nodo libro
                 datos_nodo = procesarXML(node);
-                
-                salida = salida + "\n " + node.getNodeName().toUpperCase()+" "+(contador)+": ";
-                salida = salida + "\n " + "Name: " + datos_nodo[0];
-                salida = salida + "\n " + "Price: " + datos_nodo[1];
-                salida = salida + "\n " + "Description: " + datos_nodo[2];
-                salida = salida + "\n " + "Calories: " + datos_nodo[3]+"\n";
-                contador++;
+
+                salida = salida + "\n " + node.getNodeName().toUpperCase() + " " + datos_nodo[0] + ": ";
+                salida = salida + "\n " + "Id: " + datos_nodo[0];
+                salida = salida + "\n " + "Name: " + datos_nodo[1];
+                salida = salida + "\n " + "Price: " + datos_nodo[2];
+                salida = salida + "\n " + "Description: " + datos_nodo[3];
+                salida = salida + "\n " + "Calories: " + datos_nodo[4] + "\n";
             }
         }
         return salida;
     }
 
     private String[] procesarXML(Node node) {
-        String datos[] = new String[4];
+        String datos[] = new String[5];
         Node ntemp = null;
         int contador = 0;
 //        System.out.println("node "+node);
@@ -97,13 +100,52 @@ public class XMLconDOM {
             if (ntemp.getNodeType() == Node.ELEMENT_NODE) {
                 //IMPORTANTE: para obtener el texto con el título y autor se accede al nodo //TEXT hijo de ntemp y se
                 //saca su valor.
-                
+
                 datos[contador] = ntemp.getChildNodes().item(0).getNodeValue();
 //                System.out.println("datos"+datos[contador]);
                 contador++;
             }
         }
         return datos;
+    }
+
+    public void Update(Document doc, String id, String name, String price, String description, String calories) {
+        try {
+            NodeList nl = doc.getElementsByTagName("food");
+            for (int i = 0; i < nl.getLength(); i++) {
+                Element food = (Element) nl.item(i);
+                if (food.getElementsByTagName("id").item(0).getTextContent()
+                        .equals(id)) {
+                    food.getElementsByTagName("name").item(0)
+                            .setTextContent(name);
+                    food.getElementsByTagName("price").item(0)
+                            .setTextContent(price);
+                    food.getElementsByTagName("description").item(0)
+                            .setTextContent(description);
+                    food.getElementsByTagName("calories").item(0)
+                            .setTextContent(calories);
+                }
+            }
+            //write to file
+            saveXMLContent(doc, "./src/actividadxmldom/Comida.xml");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void saveXMLContent(Document doc, String path) {
+        try {
+            TransformerFactory tff = TransformerFactory.newInstance();
+            Transformer tf = tff.newTransformer();
+            tf.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource ds = new DOMSource(doc);
+            StreamResult sr = new StreamResult(path);
+            tf.transform(ds, sr);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
